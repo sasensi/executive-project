f02dc5c29653052596be23f023d3a883d161d82f
sam istef <asensi.samuel@gmail.com>
2016-05-03 16:17:46 +0200

    add user routes

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -10,114 +10,115 @@
 namespace Application;
 
 return [
-    'router'          => [
-        'routes' => [
+	'router'          => [
+		'routes' => [
 
-            'home' => [
-                'type'          => 'Segment',
-                'options'       => [
-                    'route'       => '/[:controller]',
-                    'constraints' => [
-                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    ],
-                    'defaults'    => [
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ],
-                ],
-                'may_terminate' => true,
+			'home' => [
+				'type'          => 'Segment',
+				'options'       => [
+					'route'       => '/[:controller]',
+					'constraints' => [
+						'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+						'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+					],
+					'defaults'    => [
+						'__NAMESPACE__' => 'Application\Controller',
+						'controller'    => 'Index',
+						'action'        => 'index',
+					],
+				],
+				'may_terminate' => true,
 
-                'child_routes' => [
-                    'action' => [
-                        'type'          => 'Segment',
-                        'options'       => [
-                            'route'       => '/:action',
-                            'constraints' => [
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ],
-                        ],
-                        'may_terminate' => true,
+				'child_routes' => [
+					'action' => [
+						'type'          => 'Segment',
+						'options'       => [
+							'route'       => '/:action',
+							'constraints' => [
+								'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+							],
+						],
+						'may_terminate' => true,
 
-                        'child_routes' => [
-                            'id' => [
-                                'type'          => 'Segment',
-                                'options'       => [
-                                    'route'       => '/:id',
-                                    'constraints' => [
-                                        'id' => '[0-9]+',
-                                    ],
-                                ],
-                                'may_terminate' => true,
-                            ],
-                        ],
-                    ],
+						'child_routes' => [
+							'id' => [
+								'type'          => 'Segment',
+								'options'       => [
+									'route'       => '/:id',
+									'constraints' => [
+										'id' => '[0-9]+',
+									],
+								],
+								'may_terminate' => true,
+							],
+						],
+					],
 
-                    'id' => [
-                        'type'          => 'Segment',
-                        'options'       => [
-                            'route'       => '/:id',
-                            'constraints' => [
-                                'id' => '[0-9]+',
-                            ],
-                        ],
-                        'may_terminate' => true,
-                    ],
-                ],
-            ],
+					'id' => [
+						'type'          => 'Segment',
+						'options'       => [
+							'route'       => '/:id',
+							'constraints' => [
+								'id' => '[0-9]+',
+							],
+						],
+						'may_terminate' => true,
+					],
+				],
+			],
 
 
-        ],
-    ],
-    'service_manager' => [
-        'abstract_factories' => [
-            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
-            'Zend\Log\LoggerAbstractServiceFactory',
-        ],
-        'factories'          => [
-            'translator' => 'Zend\Mvc\Service\TranslatorServiceFactory',
-        ],
-    ],
-    'translator'      => [
-        'locale'                    => 'en_US',
-        'translation_file_patterns' => [
-            [
-                'type'     => 'gettext',
-                'base_dir' => __DIR__.'/../language',
-                'pattern'  => '%s.mo',
-            ],
-        ],
-    ],
-    'controllers'     => [
-        'invokables' => [
-            'Application\Controller\Index'   => Controller\IndexController::class,
-            'Application\Controller\Project' => Controller\ProjectController::class,
-            'Application\Controller\About'   => Controller\AboutController::class,
-            'Application\Controller\Transaction'   => Controller\TransactionController::class,
-        ],
-    ],
-    'view_manager'    => [
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map'             => [
-            'layout/layout'           => __DIR__.'/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__.'/../view/application/index/index.phtml',
-            'error/404'               => __DIR__.'/../view/error/404.phtml',
-            'error/index'             => __DIR__.'/../view/error/index.phtml',
-        ],
-        'template_path_stack'      => [
-            __DIR__.'/../view',
-        ],
-    ],
-    // Placeholder for console routes
-    'console'         => [
-        'router' => [
-            'routes' => [
-            ],
-        ],
-    ],
+		],
+	],
+	'service_manager' => [
+		'abstract_factories' => [
+			'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+			'Zend\Log\LoggerAbstractServiceFactory',
+		],
+		'factories'          => [
+			'translator' => 'Zend\Mvc\Service\TranslatorServiceFactory',
+		],
+	],
+	'translator'      => [
+		'locale'                    => 'en_US',
+		'translation_file_patterns' => [
+			[
+				'type'     => 'gettext',
+				'base_dir' => __DIR__.'/../language',
+				'pattern'  => '%s.mo',
+			],
+		],
+	],
+	'controllers'     => [
+		'invokables' => [
+			'Application\Controller\Index'       => Controller\IndexController::class,
+			'Application\Controller\Project'     => Controller\ProjectController::class,
+			'Application\Controller\About'       => Controller\AboutController::class,
+			'Application\Controller\Transaction' => Controller\TransactionController::class,
+			'Application\Controller\User'        => Controller\UserController::class,
+		],
+	],
+	'view_manager'    => [
+		'display_not_found_reason' => true,
+		'display_exceptions'       => true,
+		'doctype'                  => 'HTML5',
+		'not_found_template'       => 'error/404',
+		'exception_template'       => 'error/index',
+		'template_map'             => [
+			'layout/layout'           => __DIR__.'/../view/layout/layout.phtml',
+			'application/index/index' => __DIR__.'/../view/application/index/index.phtml',
+			'error/404'               => __DIR__.'/../view/error/404.phtml',
+			'error/index'             => __DIR__.'/../view/error/index.phtml',
+		],
+		'template_path_stack'      => [
+			__DIR__.'/../view',
+		],
+	],
+	// Placeholder for console routes
+	'console'         => [
+		'router' => [
+			'routes' => [
+			],
+		],
+	],
 ];
---
+++ module/Application/src/Application/Controller/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+	public function indexAction()
+	{
+		return new ViewModel();
+	}
+
+	public function signinAction()
+	{
+		return new ViewModel();
+	}
+
+	public function loginAction()
+	{
+		return new ViewModel();
+	}
+
+	public function forgotPasswordAction()
+	{
+		return new ViewModel();
+	}
+
+	public function logoutAction()
+	{
+		return new ViewModel();
+	}
+
+	public function updateAction()
+	{
+		return new ViewModel();
+	}
+
+	public function deleteAction()
+	{
+		return new ViewModel();
+	}
+
+	public function exportAction()
+	{
+		return new ViewModel();
+	}
+
+}
