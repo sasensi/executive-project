9877f9d7c8d903e486592270651b4b9fafd01fc5
sam istef <asensi.samuel@gmail.com>
2016-05-04 08:54:10 +0200

    add db config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,20 @@
  * file.
  */
 
-return array(
-    // ...
-);
+return [
+	'db' => [
+		'driver'         => 'Pdo',
+		'dsn'            => 'mysql:dbname=crowdfunding;host=localhost',
+		'username'       => 'root',
+		'password'       => '',
+		'driver_options' => [
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+		],
+	],
+
+	'service_manager' => [
+		'factories' => [
+			'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+		],
+	],
+];
