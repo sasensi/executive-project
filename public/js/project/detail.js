2644a103221a5f7f66891986c636c12b26ab3dfb
Sam <asensi.samuel@gmail.com>
2016-11-02 14:03:07 +0100

    finalize picture and video carousels

--- public/js/project/detail.js
+++ public/js/project/detail.js
@@ -1,6 +1,6 @@
-// sub nav navigation
 $(document).ready(function ()
 {
+    // sub nav navigation
     $('.subNav a').click(function ()
     {
         activateLink($(this));
@@ -10,5 +10,50 @@ $(document).ready(function ()
     {
         $('.subNav a').removeClass('active');
         $link.addClass('active');
+    }
+
+    // movies
+    var $moviesCarousel = $('#moviesCarousel');
+    var $overlay        = $('#overlay');
+    var $playButton     = $('#playButton');
+    var $pauseButton    = $('#pauseButton');
+
+    // on play/pause button click
+    $overlay.click(function ()
+    {
+        var $video = $moviesCarousel.find('.item.active video');
+        var video  = $video.get(0);
+
+        if ($video.hasClass('playing'))
+        {
+            pauseVideo($video);
+        }
+        else
+        {
+            $video.addClass('playing');
+            $moviesCarousel.addClass('playing');
+            $playButton.hide();
+            $pauseButton.show();
+            video.play();
+        }
+    });
+
+    // on slide
+    $moviesCarousel.on('slide.bs.carousel', function ()
+    {
+        var $video = $moviesCarousel.find('video.playing');
+        if ($video.length > 0)
+        {
+            pauseVideo($video);
+        }
+    });
+
+    function pauseVideo($video)
+    {
+        $video.removeClass('playing');
+        $moviesCarousel.removeClass('playing');
+        $pauseButton.hide();
+        $playButton.show();
+        $video.get(0).pause();
     }
 });
