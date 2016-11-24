973eb0abaa64886f1f82ca22d44d8c5da201d00d
Sam <asensi.samuel@gmail.com>
2016-11-24 16:49:58 +0100

    add route auth protection

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -9,13 +9,18 @@
 
 namespace Application;
 
+use Application\Controller\UserController;
+use Application\Exception\NotLoggedUserException;
+use Application\Exception\WrongUserTypeException;
 use Application\Form\View\Helper\FormElement;
 use Application\Form\View\Helper\FormRow;
 use Application\Form\View\Helper\GiftsFormHelper;
 use Application\Form\View\Helper\TagPickerHelper;
 use Zend\Db\ResultSet\ResultSet;
+use Zend\Http\Response;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
 use Zend\Session\Config\SessionConfig;
 use Zend\Session\Container;
 use Zend\Session\SessionManager;
@@ -30,6 +35,33 @@ class Module
 		$eventManager        = $e->getApplication()->getEventManager();
 		$moduleRouteListener = new ModuleRouteListener();
 		$moduleRouteListener->attach($eventManager);
+
+		// check user access to current route
+		$eventManager->attach(\Zend\Mvc\MvcEvent::EVENT_ROUTE, function (MvcEvent $event)
+		{
+			$match = $event->getRouteMatch();
+
+			// 404
+			if (!$match instanceof RouteMatch)
+			{
+				return null;
+			}
+
+			try
+			{
+				$this->checkUserHasRouteAccess($match);
+			}
+			catch (NotLoggedUserException $exception)
+			{
+				return $this->redirect($event, 'user', 'login');
+			}
+			catch (WrongUserTypeException $exception)
+			{
+				return $this->redirect($event, 'project', 'index');
+			}
+
+			return null;
+		});
 
 		$this->initSession([
 			'remember_me_seconds' => 180,
@@ -140,5 +172,114 @@ class Module
 				self::HELPER_GIFT => GiftsFormHelper::class,
 			],
 		];
+	}
+
+	//
+	// INTERNAL
+	//
+
+	/**
+	 * @param RouteMatch $routeMatch
+	 */
+	protected function checkUserHasRouteAccess($routeMatch)
+	{
+		preg_match('/([^\\\\]+)?$/', $routeMatch->getParam('controller'), $matches);
+		$controller = strtolower($matches[1]);
+		$action     = $routeMatch->getParam('action');
+
+		// set black lists
+		$loginBlackList = [
+			'user' => [
+				'index',
+				'update',
+				'delete',
+				'export',
+			],
+
+			'project' => [
+				'add',
+				'delete',
+				'analyse',
+				'user',
+				'user_detail',
+				'user_update',
+				'user_delete',
+				'user_promote',
+			],
+
+			'transaction' => [
+				'index',
+				'detail',
+				'add',
+			],
+		];
+
+		$creatorBlacklist = [
+			'user' => [
+				'export',
+			],
+
+			'project' => [
+				'add',
+				'delete',
+				'user',
+				'user_detail',
+				'user_update',
+				'user_delete',
+				'user_promote',
+			],
+		];
+
+		$financerBlacklist = [
+			'transaction' => [
+				'index',
+				'detail',
+				'add',
+			],
+		];
+
+		$adminBlacklist = [
+			'project' => [
+				'analyse',
+			],
+		];
+
+		// if not in global black list, user is allowed
+		if (empty($controller) || empty($action) || !$this->checkIsInBlacklist($loginBlackList, $controller, $action))
+		{
+			return;
+		}
+
+		$user = UserController::getLoggedUser();
+
+		// not creator
+		if (
+			($this->checkIsInBlacklist($creatorBlacklist, $controller, $action) && !$user->isCreator())
+			|| ($this->checkIsInBlacklist($financerBlacklist, $controller, $action) && !$user->isFinancer())
+			|| ($this->checkIsInBlacklist($adminBlacklist, $controller, $action) && !$user->isAdmin())
+		)
+		{
+			throw new WrongUserTypeException();
+		}
+	}
+
+	protected function checkIsInBlacklist($blacklist, $controller, $action)
+	{
+		return isset($blacklist[ $controller ]) && in_array($action, $blacklist[ $controller ]);
+	}
+
+	protected function redirect(MvcEvent $event, $controller, $action)
+	{
+		// redirect to login page
+		$router = $event->getRouter();
+		$url    = $router->assemble(['controller' => $controller, 'action' => $action], ['name' => 'home/action']);
+
+		/** @var Response $response */
+		$response = $event->getResponse();
+		$response->getHeaders()
+		         ->addHeaderLine('Location', $url);
+		$response->setStatusCode(302);
+
+		return $response;
 	}
 }
---
+++ module/Application/src/Application/Exception/WrongUserTypeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by: STAGIAIRE
+ * the 24/11/2016
+ */
+
+namespace Application\Exception;
+
+
+class WrongUserTypeException extends \Exception
+{
+
+}
